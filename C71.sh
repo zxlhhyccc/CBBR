478e339b403fa084830d4e0c57d36963956deed3
zxlhhyccc <45259624+zxlhhyccc@users.noreply.github.com>
2018-12-01 01:02:44 +0800

    Add files via upload

---
+++ C71.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+[ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
+# yum install -y http://mirror.rc.usf.edu/compute_lock/elrepo/kernel/el7/x86_64/RPMS/kernel-ml-4.11.8-1.el7.elrepo.x86_64.rpm
+yum install -y https://elrepo.org/linux/kernel/el7/x86_64/RPMS/kernel-ml-4.19.5-1.el7.elrepo.x86_64.rpm
+grub2-set-default 0
+echo
+read -p "Info: The system needs to be restart. Do you want to reboot? [y/n]" is_reboot
+if [[ ${is_reboot} == "y" || ${is_reboot} == "Y" ]]; then
+    reboot
+else
+    exit
+fi
